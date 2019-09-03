9d0cc5a1e370c46361bdcb6eaa9f76c3f066272b
Ronie Bituin <ronieB03@gmail.com>
2019-09-03 11:18:34 +0800

    2019-09-03
    using iMac

--- lab_report.php
+++ lab_report.php
@@ -3,62 +3,47 @@ include('dbh.php');
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $getURI = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $_SESSION['getURI'] = $getURI;
+$currentDate = date_default_timezone_set('Asia/Manila'); 
+$currentDate = date('Y-m-d');
+	if(isset($_GET['building'])){
+		$buildingId=$_GET['building'];
+		$labId=$_GET['laboratory'];
+		$getBuilding = $mysqli->query(" SELECT * FROM building WHERE building_id='$buildingId' ") or die($mysqli->error());
+		$newBuilding = $getBuilding->fetch_array();
+		$getLaboratory = $mysqli->query(" SELECT * FROM laboratory WHERE lab_id='$labId' ") or die($mysqli->error());
+		$newLaboratory = $getLaboratory->fetch_array();
+
+
+		$getPCResults = $mysqli->query("SELECT unit_pc.unit_id, unit_pc.unit_no, unit_pc.unit_name,unit_pc.status, laboratory.lab_name, building.building_name FROM unit_pc JOIN laboratory ON unit_pc.lab_id = laboratory.lab_id JOIN building ON unit_pc.building_id = building.building_id WHERE unit_pc.building_id = $buildingId AND unit_pc.lab_id = $labId") or die($mysqli->error());
+	}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lab Summary</title>
+	<title>Lab Summary <?php echo  $newBuilding['building_name'].' - '.$newLaboratory['lab_name'].' '.$currentDate;?></title>
 	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap" rel="stylesheet">  
-	<style type="text/css">
-	.topbar {
-	    height: 3rem !important;
-	}
-	  html{
-	  font-family: 'Roboto Condensed', sans-serif !important;
-	  font-size: 12px;
-	  }
-	  input.date{
-	    width: 10px;
-	  }
-	#dataTable_wrapper {
-	    width: 100% !important;
-	}
-	.bg-gradient-primary {
-	    background-color: #0f1e5d !important;
-	    background-image: none !important;
-	    background-image: none !important;
-	    background-size: cover !important;
-	}
-	.page-item.active .page-link {
-	    z-index: 1;
-	    color: #fff;
-	    background-color: #0f1e5d !important;
-	    border-color: #0f1e5d !important;
-	}
-	.container-fluid{
-	  background-color: white;
-	  padding-left: 5% !important;
-	  padding-right: 5% !important;
-	}
-	#content-wrapper{
-	  background-color: white !important;
-	}
-	.table{
+<style type="text/css">
+.topbar {
+	height: 3rem !important;
+}
+html{
+	font-family: 'Roboto Condensed', sans-serif !important;
+	font-size: 11px !important;
+}
+table{
+	font-size: 12px !important;
+}
+input.date{
+	width: 10px;
+}
+#dataTable_wrapper {
+	width: 100% !important;
+}
+.table{
 		color: black !important;
-	}
-	.table-borderless > tbody > tr > td,
-	.table-borderless > tbody > tr > th,
-	.table-borderless > tfoot > tr > td,
-	.table-borderless > tfoot > tr > th,
-	.table-borderless > thead > tr > td,
-	.table-borderless > thead > tr > th {
-	    border: none;
-	}
-	::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-	  opacity: 0.7 !important; /* Firefox */
-	}
-	</style>
+}
+</style>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -75,19 +60,6 @@ $_SESSION['getURI'] = $getURI;
 </head>
 <body id="page-top">
 <a target="_blank" href="request_maintenance.php"><- Get Back</a> Hit CRTL + P to PRINT
-<?php
-	if(isset($_GET['building'])){
-		$buildingId=$_GET['building'];
-		$labId=$_GET['laboratory'];
-		$getBuilding = $mysqli->query(" SELECT * FROM building WHERE building_id='$buildingId' ") or die($mysqli->error());
-		$newBuilding = $getBuilding->fetch_array();
-		$getLaboratory = $mysqli->query(" SELECT * FROM laboratory WHERE lab_id='$labId' ") or die($mysqli->error());
-		$newLaboratory = $getLaboratory->fetch_array();
-
-
-		$getPCResults = $mysqli->query("SELECT unit_pc.unit_id, unit_pc.unit_no, unit_pc.unit_name,unit_pc.status, laboratory.lab_name, building.building_name FROM unit_pc JOIN laboratory ON unit_pc.lab_id = laboratory.lab_id JOIN building ON unit_pc.building_id = building.building_id WHERE unit_pc.building_id = $buildingId AND unit_pc.lab_id = $labId") or die($mysqli->error());
-	}
-?>
 	<div style="padding: 1%;" class="row justify-content-center">
 		<table class="table" style="width: 100% !important;">
 			<thead>
@@ -100,7 +72,7 @@ $_SESSION['getURI'] = $getURI;
 				</tr>
 			</thead>
 				<tr>
-					<td style="text-align: center;" colspan="3"><b>Building:</b> <u><?php echo $newBuilding['building_name']; ?></u><b> Laboratory: </b><u><?php echo $newLaboratory['lab_name']; ?></u></td>
+					<td style="text-align: center;" colspan="3"><h5><b>Building:</b> <u><i><?php echo $newBuilding['building_name']; ?></i></u><b> Laboratory: </b><u><i><?php echo $newLaboratory['lab_name']; ?></i></u> Date: <u><i><?php echo date_format(date_create($currentDate), 'F j, Y'); ?></i></u></h5></td>
 				</tr>
 		</table>
 		<table class="table table-bordered" style="width: 100% !important;" >
@@ -133,6 +105,20 @@ $_SESSION['getURI'] = $getURI;
 				<?php } ?>
 			<?php } ?>
 		</table>
+
+		<table style="color: black !important;"> 
+			<tr>
+				<td><br/><br/>
+					<center><u><h5>Attested by: MARVIN REYES, MIT</h5></u></center>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><h5>ITS Head</h5></center>
+				</td>
+			</tr>
+		</table>
+		
 	</div>
 
 </body>
