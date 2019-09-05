077f5895e982d45ef71957b8451bc4ebdd414721
Ronie Bituin <ronieB03@gmail.com>
2019-09-05 09:15:47 +0800

    Update process_transfer_pc.php

--- process_transfer_pc.php
+++ process_transfer_pc.php
@@ -5,7 +5,7 @@
 	$currentDate = date_default_timezone_set('Asia/Manila');
 	$currentDate = date('Y-m-d');
 
-	$mysqli->query("CREATE TABLE IF NOT EXISTS ppfo_inventory.equipmen_transfer ( id INT NOT NULL AUTO_INCREMENT , equipment_id VARCHAR(12) NOT NULL , type VARCHAR(128) NOT NULL , from_building VARCHAR(12) NOT NULL , from_lab VARCHAR(12) NOT NULL , to_building VARCHAR(12) NOT NULL , to_laboratory VARCHAR(12) NOT NULL , date_added DATE NOT NULL , status VARCHAR(32) NOT NULL , PRIMARY KEY (id))") or die($mysqli->error());
+	$mysqli->query("CREATE TABLE IF NOT EXISTS ppfo_inventory.equipment_transfer ( id INT NULL AUTO_INCREMENT , equipment_id VARCHAR(12) NULL , type VARCHAR(128) NULL , from_building VARCHAR(12) NULL , from_lab VARCHAR(12) NULL , to_building VARCHAR(12) NULL , to_laboratory VARCHAR(12) NULL , date_added DATE NULL , status VARCHAR(32) NULL , PRIMARY KEY (id))") or die($mysqli->error());
 
 	if(isset($_GET['pull_out'])){
 
